dd29a6a860a63dbd8ac550ad63c6566b5ac4385f
vishal parashar <itsvishalparashar0119@gmail.com>
2025-06-02 12:50:32 +0530

    started a nav bar devlopment

---
+++ app/components/NavBar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const NavBar = () => {
+    return (
+        <div className='bg-blue-950 text-white p-2 flex  justify-between'>
+            <div className=" text-lg font-bold">
+                 Get me a chai?
+            </div>
+
+            <div className="">
+                <ul className='flex gap-2'>
+                    <li>home</li>
+                    <li>about</li>
+                    <li>projects</li>
+                    <li>singup</li>
+                    <li>login</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default NavBarbehind 
