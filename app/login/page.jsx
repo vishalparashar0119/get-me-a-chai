c5c37450bdf3dfa547f7abb0546297e318b870ff
vishal parashar <itsvishalparashar0119@gmail.com>
2025-06-02 18:29:27 +0530

    start working on barnch

---
+++ app/login/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (<>
+
+    <div className='text-white text-2xl font-bold py-3 px-2'>
+        <h1>Login to get you fans support</h1>
+    </div>
+  </>
+  )
+}
+
+export default page
