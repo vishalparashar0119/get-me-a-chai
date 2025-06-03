0d1515b78246687d6a14f629ebf0056274a660a5
vishal parashar <itsvishalparashar0119@gmail.com>
2025-06-03 11:46:24 +0530

    ui for login provider completed for lg:screen

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -14,7 +14,8 @@ const NavBar = () => {
 
                 <li><button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center  ">SignUp</button>
                     </li>
-                    <li><Link href="/login">
+                    <li><Link href="/login"
+                    >
                         <button type="button" className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center   ">LogIn</button></Link>
                     </li>
                 </ul>
--- app/login/page.jsx
+++ app/login/page.jsx
@@ -5,6 +5,38 @@ const page = () => {
 
     <div className='text-white text-2xl font-bold py-3 px-2'>
         <h1>Login to get you fans support</h1>
+        <div className='w-full  flex flex-col justify-center items-center gap-3 lg:py-16'>
+
+               <div className="bg-slate-800 rounded-lg hover:bg-slate-700 py-2  w-1/5 flex gap-2 justify-center items-center ">
+                  <img src="/auth/google.svg" alt="" width={30} height={30} />
+                  <span className='text-sm font-semibold'> Continue with google</span>
+               </div>
+
+               <div className="bg-slate-800 rounded-lg hover:bg-slate-700 py-2 w-1/5 flex gap-2 justify-center items-center ">
+                  <img src="/auth/facebook.svg" alt="" width={30} height={30} />
+                  <span className='text-sm font-semibold'> Continue with facebook</span>
+               </div>
+
+               <div className="bg-slate-800 rounded-lg hover:bg-slate-700 py-2 w-1/5 flex gap-2 justify-center items-center ">
+                  <img src="/auth/twitter.svg" alt="" width={30} height={30} />
+                  <span className='text-sm font-semibold'> Continue with twitter</span>
+               </div>
+
+               <div className="bg-slate-800 rounded-lg hover:bg-slate-700 py-2 w-1/5 flex gap-2 justify-center items-center ">
+                  <img src="/auth/linkedln.svg" alt="" width={30} height={30} />
+                  <span className='text-sm font-semibold'> Continue with linkedln</span>
+               </div>
+
+               <div className="bg-slate-800 rounded-lg hover:bg-slate-700 py-2 w-1/5 flex gap-2 justify-center items-center ">
+                  <img src="/auth/github.svg" alt="" width={30} height={30} />
+                  <span className='text-sm font-semibold'> Continue with github</span>
+               </div>
+
+               <div className="bg-slate-800 rounded-lg hover:bg-slate-700 py-2 w-1/5 flex gap-2 justify-center items-center ">
+                  <img src="/auth/apple.svg" alt="" width={30} height={30} />
+                  <span className='text-sm font-semibold'> Continue with apple</span>
+               </div>
+        </div>
     </div>
   </>
   )
