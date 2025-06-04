0c38f19b67d9ca272391b8a5226e64e816824cad
vishal parashar <itsvishalparashar0119@gmail.com>
2025-06-04 16:29:41 +0530

    create a dashboard component with form handling

--- app/[userName]/page.jsx
+++ app/[userName]/page.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const page = async ({ params }) => {
-  const { userName } = await  params;
+  const { userName } = await params;
   return (
     <div className=' text-white'>
       <div className='w-full h-[55vh] relative'>
@@ -15,18 +15,52 @@ const page = async ({ params }) => {
       <div className='flex justify-center items-center flex-col my-20 gap-2'>
         <h1 className='lg:text-4xl font-bold'>{userName}</h1>
         <p className='lg:text-sm'>creating Linux, Coding, and Technology Tutorials/Rant Videos</p>
-         <ul className='list-disc list-inside flex gap-2 text-sm opacity-50 '>
+        <ul className='list-disc list-inside flex gap-2 text-sm opacity-50 '>
           <li>232 members</li>
           <li>1 Post</li>
           <li>$387.9/month</li>
-         </ul>
+        </ul>
       </div>
 
-      <div>
-        
+      <div className='w-4/5 mx-auto flex gap-2 '>
+
+        <div className="w-1/2 bg-slate-900 min-h-[100px] max-h-[200px] p-5 rounded-lg overflow-y-auto">
+          <h1 className='text-xl font-bold'>Supporters</h1>
+          <ul className='pl-2'>
+            <li><span className='text-lg font-bold text-slate-500'>vishal</span> : donate <span className='text-green-500'>10$</span> with a message : "<span className='text-amber-400'>we support you bhai ❤️</span>"</li>
+
+          </ul></div>
+        <div className="w-1/2 bg-slate-900 p-5 rounded-lg">
+          <h1 className='text-xl font-bold'>Make a payment</h1>
+          <div className='mt-5 flex flex-col gap-3 '>
+            <div className='flex flex-col gap-3'>
+              <input type="text" className='bg-slate-800 p-2 w-full rounded-sm' placeholder='Enter Name ' />
+              <input type="text" className='bg-slate-800 p-2 w-full rounded-sm' placeholder='Enter Message' />
+              <input type="text" className='bg-slate-800 p-2 w-full rounded-sm' placeholder='Enter Amount' />
+            </div>
+
+            <div className='w-1/4'>
+              <button type="button" className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center w-full cursor-pointer" >Pay</button>
+            </div>
+
+            <div className='flex gap-2' >
+              <button className='bg-slate-700 py-2 px-5 rounded-lg cursor-pointer '>
+                $5
+              </button>
+              <button className='bg-slate-700 py-2 px-5 rounded-lg cursor-pointer '>
+                $10
+              </button>
+              <button className='bg-slate-700 py-2 px-5 rounded-lg cursor-pointer '>
+                $20
+              </button>
+            </div>
+          </div>
+        </div>
+
+
       </div>
 
-      
+
 
 
     </div>
--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,9 +1,29 @@
 "use client"
-import React , {useEffect}from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
 
 const page = () => {
+
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    userName: '',
+    profilePic: '',
+    coverPic: '',
+    razorPayId: '',
+    razorPaySecret: ''
+
+  });
+
+  const handleChange = (e)=>{
+      setFormData({...formData , [e.target.name] : e.target.value})
+  }
+
+  const submitForm = ()=>{
+     console.log(formData);
+     alert("submit successfull")
+  }
      const { data: session, status } = useSession();
   const router = useRouter();
 
@@ -18,7 +38,52 @@ const page = () => {
   }
 
   if (status === 'authenticated') {
-    return <div className='text-white'>This is the dashboard page</div>;
+  return <div className='text-white flex items-center flex-col'>
+    <h1 className='lg:text-3xl font-bold my-5'>welcome to your dashboard</h1>
+    <div className="lg:w-2/4 flex flex-col items-center gap-2">
+      {/*name*/}
+      <div className='w-full flex flex-col gap-2'>
+        <label htmlFor="" className=''>Name</label>
+        <input onChange={handleChange} type="text" className='w-full p-1.5 bg-slate-700 rounded-sm' name='name' value={formData.name} />
+      </div>
+      {/*email*/}
+      <div className='w-full flex flex-col gap-2'>
+        <label htmlFor="" className=''>Email</label>
+        <input onChange={handleChange} type="text" className='w-full p-1.5 bg-slate-700 rounded-sm' value={formData.email} name='email' />
+      </div>
+      {/*userName*/}
+      <div className='w-full flex flex-col gap-2'>
+        <label htmlFor="" className=''>User Name</label>
+        <input onChange={handleChange} type="text" className='w-full p-1.5 bg-slate-700 rounded-sm' value={formData.userName}  name='userName'/>
+      </div>
+      {/*profile pitchure*/}
+      <div className='w-full flex flex-col gap-2'>
+        <label htmlFor="" className=''>Profile Picture</label>
+        <input onChange={handleChange} type="text" className='w-full p-1.5 bg-slate-700 rounded-sm' value={formData.profilePic} name='profilePic' />
+      </div>
+      {/*cover pic*/}
+      <div className='w-full flex flex-col gap-2'>
+        <label htmlFor="" className=''>Cover Picture</label>
+        <input onChange={handleChange} type="text" className='w-full p-1.5 bg-slate-700 rounded-sm' value={formData.coverPic} name='coverPic'/>
+      </div>
+      {/*input razor pay id*/}
+      <div className='w-full flex flex-col gap-2'>
+        <label htmlFor="" className=''>RazorPay Id</label>
+        <input onChange={handleChange} type="text" className='w-full p-1.5 bg-slate-700 rounded-sm' value={formData.razorPayId} name='razorPayId' />
+      </div>
+      {/*input razor pay secret*/}
+      <div className='w-full flex flex-col gap-2'>
+        <label htmlFor="" className=''>RazorPay Secret</label>
+        <input onChange={handleChange} type="text" className='w-full p-1.5 bg-slate-700 rounded-sm' value={formData.razorPaySecret} name='razorPaySecret' />
+      </div>
+      {/* submit button */}
+      <div className='w-1/4 mt-1'>
+        <button onClick={submitForm} type="button" className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center w-full">Save</button>
+      </div>
+
+    </div>
+
+  </div>;
   }
 }
 
